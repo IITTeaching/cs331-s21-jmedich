5ce25940d0750223e71a702ac6ef12416b967122
Justin Medich <56899391+Rantsycancy@users.noreply.github.com>
2021-05-12 20:25:35 -0500

    Update radix-sort.py

--- project/radix-sort.py
+++ project/radix-sort.py
@@ -7,4 +7,31 @@ def book_to_words(book_url='https://www.gutenberg.org/files/84/84-0.txt'):
     return bookascii.split()
 
 def radix_a_book(book_url='https://www.gutenberg.org/files/84/84-0.txt'):
-    pass
+    lst = book_to_words()
+    largestlen = len(max(lst,key = len)) - 2
+    rSorted = countSort(lst,largestlen + 1)
+    for i in range(largestlen, -1, -1):
+        rSorted = countSort(rSorted, i)
+    return rSorted
+
+def countSort(lst, digit):
+    count = [0] * 128
+    sorted = [None] * len(lst)
+    for i in lst:
+        if len(i) - 1 < digit:
+            k = 0
+        else:
+            k = ord(i.decode('ascii')[digit])
+        count[k] += 1
+    for i in range(1, len(count)):
+        count[i] += count[i - 1]
+    for i in range(len(lst) - 1, -1, -1):
+        if len(lst[i]) - 1 < digit:
+            k = 0
+        else:
+            k = ord(lst[i].decode('ascii')[digit])
+        sorted[count[k] - 1] = lst[i]
+        count[k] += -1
+    return sorted
+
+print(radix_a_book())
